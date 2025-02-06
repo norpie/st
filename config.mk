8dd02df9a19092cc44ca2a427256fdf2cd7051cd
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-06 23:44:02 +0100

    build: enabled correct build features

--- config.mk
+++ config.mk
@@ -15,7 +15,7 @@ X11LIB = /usr/X11R6/lib
 PKG_CONFIG = pkg-config
 
 # Uncomment this for the alpha patch / ALPHA_PATCH
-#XRENDER = `$(PKG_CONFIG) --libs xrender`
+XRENDER = `$(PKG_CONFIG) --libs xrender`
 
 # Uncomment this for the themed cursor patch / THEMED_CURSOR_PATCH
 #XCURSOR = `$(PKG_CONFIG) --libs xcursor`
@@ -27,22 +27,22 @@ PKG_CONFIG = pkg-config
 #LIGATURES_LIBS = `$(PKG_CONFIG) --libs harfbuzz`
 
 # Uncomment this for the SIXEL patch / SIXEL_PATCH
-#SIXEL_C = sixel.c sixel_hls.c
-#SIXEL_LIBS = `$(PKG_CONFIG) --libs imlib2`
+SIXEL_C = sixel.c sixel_hls.c
+SIXEL_LIBS = `$(PKG_CONFIG) --libs imlib2`
 
 # Uncomment for the netwmicon patch / NETWMICON_PATCH
 #NETWMICON_LIBS = `$(PKG_CONFIG) --libs gdlib`
 
 # includes and libs, uncomment harfbuzz for the ligatures patch
 INCS = -I$(X11INC) \
-       `$(PKG_CONFIG) --cflags fontconfig` \
-       `$(PKG_CONFIG) --cflags freetype2` \
-       $(LIGATURES_INC)
+	   `$(PKG_CONFIG) --cflags fontconfig` \
+	   `$(PKG_CONFIG) --cflags freetype2` \
+	   $(LIGATURES_INC)
 LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft ${SIXEL_LIBS} ${XRENDER} ${XCURSOR}\
-       `$(PKG_CONFIG) --libs fontconfig` \
-       `$(PKG_CONFIG) --libs freetype2` \
-       $(LIGATURES_LIBS) \
-       $(NETWMICON_LIBS)
+	   `$(PKG_CONFIG) --libs fontconfig` \
+	   `$(PKG_CONFIG) --libs freetype2` \
+	   $(LIGATURES_LIBS) \
+	   $(NETWMICON_LIBS)
 
 # flags
 STCPPFLAGS = -DVERSION=\"$(VERSION)\" -DICON=\"$(ICONPREFIX)/$(ICONNAME)\" -D_XOPEN_SOURCE=600
@@ -52,8 +52,8 @@ STLDFLAGS = $(LIBS) $(LDFLAGS)
 # OpenBSD:
 #CPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600 -D_BSD_SOURCE
 #LIBS = -L$(X11LIB) -lm -lX11 -lutil -lXft \
-#       `pkg-config --libs fontconfig` \
-#       `pkg-config --libs freetype2`
+#		`pkg-config --libs fontconfig` \
+#		`pkg-config --libs freetype2`
 #MANPREFIX = ${PREFIX}/man
 
 # compiler and linker
